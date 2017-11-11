0b067dd89c0b5b69391d0274c2853cd23032b378
INAGAKI Hiroshi <musashino.open@gmail.com>
2017-11-11 10:51:45 +0900

    ramips: Fix DEVICE_TITLE for WN-G300DGR

--- target/linux/ramips/image/rt305x.mk
+++ target/linux/ramips/image/rt305x.mk
@@ -789,7 +789,7 @@ define Device/wn-g300dgr
   DTS := WN-G300DGR
   BLOCKSIZE := 64k
   IMAGE_SIZE := $(ralink_default_fw_size_4M)
-  DEVICE_TITLE := IO-DATA WN-G300DGR
+  DEVICE_TITLE := IODATA WN-G300DGR
   DEVICE_PACKAGES := kmod-switch-rtl8366rb kmod-swconfig swconfig \
     kmod-usb-core kmod-usb-dwc2 kmod-usb2 kmod-usb-ohci
 endef
